9dca6793c23a9e37a912ad2faaba1b386be694ee
Michael Bock <no-day@posteo.net>
2022-10-02 13:54:23 +0200

    Update packages

--- src-ts-gen/SampleTsGen/TsBridgeClass.purs
+++ src-ts-gen/SampleTsGen/TsBridgeClass.purs
@@ -1,4 +1,8 @@
-module SampleTsGen.TsBridgeClass where
+module MyApp.TsBridgeClass where
+
+{-GEN:imports
+{ autoPrefix: "auto" }
+-}
 
 import Data.Either (Either)
 import Heterogeneous.Mapping (class Mapping)
@@ -6,6 +10,8 @@ import SampleApp.Types as SampleApp.Types
 import TsBridge (TsBridgeM, TsType)
 import TsBridge.Class (defaultProxy, tsOpaqueType, tsOpaqueType2)
 import Type.Proxy (Proxy)
+
+{-GEN:END-}
 
 class ToTsBridge a where
   toTsBridge :: a -> TsBridgeM TsType
@@ -18,11 +24,14 @@ instance ToTsBridge a => Mapping MappingToTsBridge a (TsBridgeM TsType) where
 instance ToTsBridge a => ToTsBridge (Proxy a) where
   toTsBridge = defaultProxy Mp
 
-
 instance (ToTsBridge a, ToTsBridge b) => ToTsBridge (Either a b) where
   toTsBridge = tsOpaqueType2 Mp "Data.Either" "Either" "A" "B"
 
-{-GEN:instances-}
+{-GEN:instances
+{ include: "",
+, exclude: ""
+}
+-}
 
 instance ToTsBridge SampleApp.Types.AppState where
   toTsBridge = tsOpaqueType "SampleApp.Types" "AppState"
